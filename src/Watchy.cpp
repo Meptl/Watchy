1968e15feb51671a14d6c67f72c6c6f26061d331
Christopher Chin <ctchin13@gmail.com>
2021-12-20 17:12:32 +0000

    Update default hourglass time.

--- src/Watchy.cpp
+++ src/Watchy.cpp
@@ -3,7 +3,7 @@
 WatchyRTC Watchy::RTC;
 GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> Watchy::display(GxEPD2_154_D67(CS, DC, RESET, BUSY));
 
-RTC_DATA_ATTR int hourglassMinutes = 8;
+RTC_DATA_ATTR int hourglassMinutes = 20;
 RTC_DATA_ATTR int targetMinute;
 RTC_DATA_ATTR int guiState;
 RTC_DATA_ATTR int menuIndex;
